c445c565f2641235cff69ca0b91b0dd198f1af4b
Takafumi Arakaki <aka.tkf@gmail.com>
2018-02-18 17:03:02 -0800

    Test more add_kwonly exceptions

--- test/test_add_kwonly.jl
+++ test/test_add_kwonly.jl
@@ -1,6 +1,6 @@
 module TestAddKwonly
 using Base.Test
-using Reconstructables: @add_kwonly, add_kwonly, UndefKeywordError
+using Reconstructables: @add_kwonly, UndefKeywordError
 
 @add_kwonly function f(a, b; c=3, d=4)
   (a, b, c, d)
@@ -16,5 +16,35 @@ end
 @add_kwonly h(; c=3, d=4) = (c, d)
 @test h() == (3, 4)
 
-@test_throws ErrorException add_kwonly(:(i(c=3, d=4) = (c, d)))
+
+macro test_error(testee, tester)
+    quote
+        tester = $(esc(tester))
+        err = try
+            $(esc(testee))
+        catch err
+            err
+        end
+        tester(err)
+    end
+end
+
+@test_error begin
+    @eval @add_kwonly i(c=3, d=4) = (c, d)
+end (err) -> begin
+    @test err isa ErrorException
+    @test contains(err.msg,
+                   "At least one positional mandatory argument is required")
+end
+
+@test_error begin
+    @eval @add_kwonly if false
+        g(a, b; c=3, d=4) = (a, b, c, d)
+    end
+end (err) -> begin
+    @test err isa ErrorException
+    @test contains(err.msg,
+                   "add_only does not work with expression if")
+end
+
 end
